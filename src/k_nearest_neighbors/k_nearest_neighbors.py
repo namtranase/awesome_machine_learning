f5cdef19334eca2fa6d888e91a2224bb32c2897c
tranducnamASE <trannam.ase@gmail.com>
2020-11-07 09:27:43 +0700

    update: KNN algorithm

---
+++ src/k_nearest_neighbors/k_nearest_neighbors.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import logging
+
+import numpy as np
+import pandas as pd
+
+import settings
+from src.config.config import read_config_file
+
+
+def process_data():
+    """Process KNN program.
+    """
+
+def main():
+    """Main program for KNN program.
+    """
+    config = read_config_file(settings.config_file)
+    if config['debug']:
+        logging.basicConfig(level=logging.DEBUG)
+    logging.debug('Start KNN with config: %s', config)
+    process_data()
+
+if __name__ == "__main__":
+    main()
