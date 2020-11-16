5cd4599db98213624ed900a90505d5a5ff5c69d1
tranducnamASE <trannam.ase@gmail.com>
2020-11-16 15:45:26 +0700

    update: prepare kmeans

--- src/k_means_clustering/k_means.py
+++ src/k_means_clustering/k_means.py
@@ -3,13 +3,32 @@ import logging
 from time import time
 
 import numpy as np
+import scipy.spatial.distance import cdist
+import random
 
 import settings
 from src.config.config import read_config_file
 
+def prepare_data():
+    """Prepare data for training.
+    """
+    means = [[2, 2], [8, 3], [3, 6]]
+    cov = [[1, 0], [0, 1]]
+    N = 500
+    X0 = np.random.multivariate_normal(mean[0], cov, N)
+    X1 = np.random.multivariate_normal(mean[1], cov, N)
+    X2 = np.random.multivariate_normal(mean[2], cov, N)
+
+    X = np.concatenate((X0, X1, X2), axis=0)
+    # Define 3 clusters
+    K = 3
+    original_labes = np.asarray([0]*N + [1]*N + [2]*N).T
+
 def process_data():
     """Process KNN program.
     """
+    np.random.seed(18)
+
 
 def main():
     """Main program for KNN program.
