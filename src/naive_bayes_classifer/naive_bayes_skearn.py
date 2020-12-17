d960aed237d41c81ef0dc1dec99a0180dfe10a9e
namtranase <trannam.ase@gmail.com>
2020-12-17 15:35:58 +0700

    update: naive bayes program

--- src/naive_bayes_classifer/naive_bayes_skearn.py
+++ src/naive_bayes_classifer/naive_bayes_skearn.py
@@ -13,64 +13,8 @@ from src.config.config import read_config_file
 def prepare_data():
     """Prepare data for training.
     """
-    means = [[2, 2], [8, 3], [3, 6]]
-    cov = [[1, 0], [0, 1]]
-    N = 500
-    X0 = np.random.multivariate_normal(means[0], cov, N)
-    X1 = np.random.multivariate_normal(means[1], cov, N)
-    X2 = np.random.multivariate_normal(means[2], cov, N)
 
-    X = np.concatenate((X0, X1, X2), axis=0)
-
-    original_labels = np.asarray([0]*N + [1]*N + [2]*N).T
-
-    return X, original_labels
-
-def kmeans_init_centroids(X, k):
-    """Randomly choose k rows of X as centroids.
-    """
-    return X[np.random.choice(X.shape[0], k, replace=False)]
-
-def kmeans_assign_labels(X, centroids):
-    """Calculate pairwise distances btw data and centroids.
-    """
-    D = cdist(X, centroids)
-
-    return np.argmin(D, axis=1)
-
-def has_converged(centroids, new_centroids):
-    """Check the new centroid is whether smaller
-    or bigger than the old one.
-    """
-    return (set([tuple(a) for a in centroids]) ==
-        set([tuple(b) for b in new_centroids]))
-
-def kmeans_update_centroids(X, labels, K):
-    """Update the new centroids for data.
-    """
-    centroids = np.zeros((K, X.shape[1]))
-    for k in range (K):
-        X_k = X[labels==k, :]
-        centroids[k,:] = np.mean(X_k, axis=0)
-
-    return centroids
-
-def kmeans(X, K):
-    """Implement kmeans algorithm in easy way.
-    """
-    centroids = [kmeans_init_centroids(X, K)]
-    labels = []
-    it = 0
-    while True:
-        logging.debug('Iteration: %s', it)
-        labels.append(kmeans_assign_labels(X, centroids[-1]))
-        new_centroids = kmeans_update_centroids(X, labels[-1], K)
-        if has_converged(centroids[-1], new_centroids):
-            break
-        centroids.append(new_centroids)
-        it += 1
-
-    return (centroids, labels, it)
+    return None
 
 def process_data():
     """Process KNN program.
@@ -93,7 +37,7 @@ def process_data():
     logging.debug('Centrel found by sklearn kmeans: %s', model.cluster_centers_)
 
 def main():
-    """Main program for KNN program.
+    """Main program for Naive bayes program.
     """
     config = read_config_file(settings.config_file)
     if config['debug']:
